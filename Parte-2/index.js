197cca4d6dda516690a2b4c42661e9d7f8bad9c3
NissimBet <nissimbet123@outlook.com>
2020-01-29 08:08:17 -0600

    parte 2 y 3 acabadas

--- Parte-2/index.js
+++ Parte-2/index.js
@@ -1,55 +1,91 @@
-let express = require( 'express' );
-let bodyParser = require( 'body-parser' );
-let mongoose = require( 'mongoose' );
+let express = require("express");
+let bodyParser = require("body-parser");
+let morgan = require("morgan");
+let mongoose = require("mongoose");
 let jsonParser = bodyParser.json();
-let { DATABASE_URL, PORT } = require( './config' );
+let { DATABASE_URL, PORT } = require("./config");
+const { updateBookmark, createBookmark } = require("./model");
 
 let app = express();
 
 let server;
 
-/* Tu código va aquí */
+app.use(morgan("default"));
+app.use(jsonParser);
 
-function runServer( port, databaseUrl ){
-	return new Promise( (resolve, reject ) => {
-		mongoose.connect( databaseUrl, response => {
-			if ( response ){
-				return reject( response );
-			}
-			else{
-				server = app.listen(port, () => {
-					console.log( "App is running on port " + port );
-					resolve();
-				})
-				.on( 'error', err => {
-					mongoose.disconnect();
-					return reject(err);
-				})
-			}
-		});
-	});
+app.put("/api/bookmarks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { titulo, descripcion, url, id: bodyId } = req.body;
+
+  if (!bodyId) {
+    res.statusMessage = "No se envio el id del bookmark";
+    return res.sendStatus(406);
+  }
+  if (id != bodyId) {
+    res.statusMessage = "Ids no coinciden";
+    return res.sendStatus(409);
+  }
+  if (!(titulo || descripcion || url)) {
+    res.statusMessage = "No se enviaron datos a modificar";
+    res.sendStatus(406);
+  }
+
+  const data = {};
+  titulo ? (data.titulo = titulo) : "";
+  descripcion ? (data.descripcion = descripcion) : "";
+  url ? data.url : "";
+
+  try {
+    const newBookmark = await updateBookmark(bodyId, data);
+    res.status(200).json(newBookmark);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+});
+
+function runServer(port, databaseUrl) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(
+      databaseUrl,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      response => {
+        if (response) {
+          return reject(response);
+        } else {
+          server = app
+            .listen(port, () => {
+              console.log("App is running on port " + port);
+              resolve();
+            })
+            .on("error", err => {
+              mongoose.disconnect();
+              return reject(err);
+            });
+        }
+      }
+    );
+  });
 }
 
-function closeServer(){
-	return mongoose.disconnect()
-		.then( () => {
-			return new Promise( (resolve, reject) => {
-				console.log( 'Closing the server' );
-				server.close( err => {
-					if ( err ){
-						return reject( err );
-					}
-					else{
-						resolve();
-					}
-				});
-			});
-		});
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing the server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  });
 }
-runServer( PORT, DATABASE_URL );
+runServer(PORT, DATABASE_URL);
 
-module.exports = { 
-    app, 
-    runServer, 
-    closeServer 
-}
+module.exports = {
+  app,
+  runServer,
+  closeServer
+};
--- Parte-2/model.js
+++ Parte-2/model.js
@@ -1,10 +1,25 @@
-let mongoose = require( 'mongoose' );
-let uuid = require( 'uuid' );
+let mongoose = require("mongoose");
+let uuid = require("uuid");
 
 mongoose.Promise = global.Promise;
 
-/* Tu código va aquí */
+const BookmarkSchema = new mongoose.Schema({
+  id: { type: String, default: uuid.v4() },
+  titulo: String,
+  descripcion: String,
+  url: String
+});
+
+const Bookmark = mongoose.model("bookmarks", BookmarkSchema);
 
 module.exports = {
-    
+  updateBookmark: async (id, data) => {
+    try {
+      await Bookmark.updateOne({ id: id }, { ...data });
+      const updatedBookmark = Bookmark.findOne({ id: id });
+      return updatedBookmark;
+    } catch (error) {
+      throw Error(error);
+    }
+  }
 };
